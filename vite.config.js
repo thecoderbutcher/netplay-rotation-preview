b8b3c3c3af239efd30057f201f393445cc732a81
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2026-02-08 03:31:14 -0300

    build test

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "docs",
+  },
 });
